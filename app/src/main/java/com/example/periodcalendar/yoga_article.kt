f430c4a762f7b490b2bfc2ae03424f111cadfb47
xxsimpnaton git <nikolatief11@gmail.com>
2021-06-26 23:34:01 +0700

    Create yoga_artikel.kt

---
+++ app/src/main/java/com/example/periodcalendar/yoga_article.kt
@@ -0,0 +1,11 @@
+package com.example.periodcalendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class yoga_article : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yoga_article)
+    }
+}
